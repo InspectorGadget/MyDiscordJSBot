0fbce5759649356927d36b7498b0386fad1628bd
RTG <igadget28@gmail.com>
2017-10-09 20:34:25 +0800

    Updated the BAT File

--- windows-installation/part1.bat
+++ windows-installation/part1.bat
@@ -2,3 +2,5 @@
 title "BOT SETUP PART 1"
 echo "If this installation fails, please install NPM and try again!"
 npm init
+if %errorlevel%=9009 echo "Please install Node.js!"
+pause
--- windows-installation/part2.bat
+++ windows-installation/part2.bat
@@ -1,4 +1,6 @@
 @echo off
-title "BOT SETUP PART 2"
+title "BOT SETUP PART 1"
 echo "If this installation fails, please run part1.bat first"
 npm install discord.js --save
+if %errorlevel%==9009 echo Command Not Found, please install Node.js
+pause
